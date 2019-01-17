d665bdf1ab24691d326d440f6687aa323abba2d1
Janden.Ma <516113525@qq.com>
2019-01-17 11:08:40 +0800

    use grid of Material-UI to instead of custom flexible box

--- client/src/components/BlogList/index.js
+++ client/src/components/BlogList/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import {
-    Card, CardActionArea, CardMedia, CardContent, CardActions, Typography, Button
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    CardActions,
+    Typography,
+    Grid,
+    Tooltip,
+    Button
 } from '@material-ui/core'
 import './index.css';
 
@@ -8,143 +16,207 @@ export default class BlogList extends Component {
     render() {
         return (
             <div className="blog_list">
-            {/* the codes below me is the static codes for test using */}
-            <Card className="blog_item">
-                <CardActionArea>
-                    <CardMedia className="blog_item_media"
-                        image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/desklampnew.jpg"
-                        title="Mi LED Desk Lamp"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                        Mi LED Desk Lamp
+                <Grid
+                    spacing={32}
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    wrap="wrap">
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/desklampnew.jpg"
+                                    title="Mi LED Desk Lamp"
+                                />
+                                <CardContent>
+                                    <Tooltip title="Mi LED Desk Lamp">
+                                        <Typography gutterBottom variant="h6" className="blog_item_title">
+                                            Mi LED Desk Lamp
+                                    </Typography>
+                                    </Tooltip>
+                                    <Typography component="p">
+                                        Intuitive brightness and color temperature adjustment
+                Works with the Google Assistant
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Mi-box-s.jpg"
+                                    title="Mi Box S"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Box S
+                                     </Typography>
+                                    <Typography component="p">
+                                        4K Ultra HD Streaming Media Player
+                Google Assistant | Chromecast built-in
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/lazerprojectnew.jpg"
+                                    title="Mi Laser Projector 150"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Laser Projector 150
+                                     </Typography>
+                                    <Typography component="p">
+                                        Ultra-short throw distance
+                A massive screen for your home
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Scooter-banner.jpg"
+                                    title="Mi Electric Scooter"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Electric Scooter
+                                     </Typography>
+                                    <Typography component="p">
+                                        Intuitive and easy-to-learn,
+                18.6 miles long-range battery life
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/desklampnew.jpg"
+                                    title="Mi LED Desk Lamp"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi LED Desk Lamp
                         </Typography>
-                        <Typography component="p">
-                        Intuitive brightness and color temperature adjustment
-Works with the Google Assistant 
+                                    <Typography component="p">
+                                        Intuitive brightness and color temperature adjustment
+                Works with the Google Assistant
                         </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            <Card className="blog_item">
-                <CardActionArea>
-                    <CardMedia className="blog_item_media"
-                        image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Mi-box-s.jpg"
-                        title="Mi Box S"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                        Mi Box S
-                        </Typography>
-                        <Typography component="p">
-                        4K Ultra HD Streaming Media Player
-Google Assistant | Chromecast built-in 
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            <Card className="blog_item">
-                <CardActionArea>
-                    <CardMedia className="blog_item_media"
-                        image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/lazerprojectnew.jpg"
-                        title="Mi Laser Projector 150"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                        Mi Laser Projector 150
-                        </Typography>
-                        <Typography component="p">
-                        Ultra-short throw distance
-A massive screen for your home 
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-            <Card className="blog_item">
-                <CardActionArea>
-                    <CardMedia className="blog_item_media"
-                        image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Scooter-banner.jpg"
-                        title="Mi Electric Scooter"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                        Mi Electric Scooter
-                        </Typography>
-                        <Typography component="p">
-                        Intuitive and easy-to-learn,
-18.6 miles long-range battery life
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-                <Card className="blog_item">
-                    <CardActionArea>
-                        <CardMedia className="blog_item_media"
-                            image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/desklampnew.jpg"
-                            title="Mi LED Desk Lamp"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                            Mi LED Desk Lamp
-                            </Typography>
-                            <Typography component="p">
-                            Intuitive brightness and color temperature adjustment
-Works with the Google Assistant 
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card className="blog_item">
-                    <CardActionArea>
-                        <CardMedia className="blog_item_media"
-                            image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Mi-box-s.jpg"
-                            title="Mi Box S"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                            Mi Box S
-                            </Typography>
-                            <Typography component="p">
-                            4K Ultra HD Streaming Media Player
-Google Assistant | Chromecast built-in 
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card className="blog_item">
-                    <CardActionArea>
-                        <CardMedia className="blog_item_media"
-                            image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/lazerprojectnew.jpg"
-                            title="Mi Laser Projector 150"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                            Mi Laser Projector 150
-                            </Typography>
-                            <Typography component="p">
-                            Ultra-short throw distance
-A massive screen for your home 
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card className="blog_item">
-                    <CardActionArea>
-                        <CardMedia className="blog_item_media"
-                            image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Scooter-banner.jpg"
-                            title="Mi Electric Scooter"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h3" className="blog_item_title">
-                            Mi Electric Scooter
-                            </Typography>
-                            <Typography component="p">
-                            Intuitive and easy-to-learn,
-18.6 miles long-range battery life
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Mi-box-s.jpg"
+                                    title="Mi Box S"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Box S
+                                     </Typography>
+                                    <Typography component="p">
+                                        4K Ultra HD Streaming Media Player
+                Google Assistant | Chromecast built-in
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/lazerprojectnew.jpg"
+                                    title="Mi Laser Projector 150"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Laser Projector 150
+                                     </Typography>
+                                    <Typography component="p">
+                                        Ultra-short throw distance
+                A massive screen for your home
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid md={4} sm={6} xs={12} item>
+                        <Card className="blog_item">
+                            <CardActionArea>
+                                <CardMedia className="blog_item_media"
+                                    image="https://i01.appmifile.com/webfile/globalimg/us/USBanner/Scooter-banner.jpg"
+                                    title="Mi Electric Scooter"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" className="blog_item_title">
+                                        Mi Electric Scooter
+                                     </Typography>
+                                    <Typography component="p">
+                                        Intuitive and easy-to-learn,
+                18.6 miles long-range battery life
+                                     </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="secondary" className="blog_readmore">
+                                    Read More &ensp;>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
